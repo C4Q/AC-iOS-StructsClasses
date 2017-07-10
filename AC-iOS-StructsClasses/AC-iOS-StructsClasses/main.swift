5ab5ee2f3e5d0588e4998ffd1322e6e1f9e2ad7f
ericastevens <ericastevens99@gmail.com>
2017-07-10 17:26:55 -0400

    Added Starter Project

---
+++ AC-iOS-StructsClasses/AC-iOS-StructsClasses/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AC-iOS-StructsClasses
+//
+//  Created by Erica Y Stevens on 7/10/17.
+//  Copyright © 2017 Erica Y Stevens. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
