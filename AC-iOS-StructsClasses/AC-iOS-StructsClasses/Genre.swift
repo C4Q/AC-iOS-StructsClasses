52ce6f43e8e65d3dc028d3cdfca7f5ffd3208e66
ericastevens <ericastevens99@gmail.com>
2017-07-11 16:32:16 -0400

    Added Other Objects

---
+++ AC-iOS-StructsClasses/AC-iOS-StructsClasses/Genre.swift
@@ -0,0 +1,13 @@
+//
+//  Genre.swift
+//  AC-iOS-StructsClasses
+//
+//  Created by Erica Y Stevens on 7/11/17.
+//  Copyright © 2017 Erica Y Stevens. All rights reserved.
+//
+
+import Foundation
+
+enum Genre {
+    
+}
