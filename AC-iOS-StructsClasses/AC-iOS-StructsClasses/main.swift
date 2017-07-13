4a2d034e6b2a2c339731aa51f26b250b2dd8eacd
ericastevens <ericastevens99@gmail.com>
2017-07-12 23:20:08 -0400

    Completed solution version project

--- AC-iOS-StructsClasses/AC-iOS-StructsClasses/main.swift
+++ AC-iOS-StructsClasses/AC-iOS-StructsClasses/main.swift
@@ -82,6 +82,19 @@ print("\nPART 1 *****************")
 
 var swiftMovies: [Movie] = []
 
+//For each movie in the Movie array, print the name of each movie and associated cast on a single line. Be sure not to print the array of cast members, only the string elements.
+/*Expeted Output:
+ Minions: Sandra Bullock, Jon Hamm, Michael Keaton
+ Shrek: Mike Myers, Eddie Murphy, Cameron Diaz
+ .
+ .
+ .
+ */
+
+//for movie in swiftMovies {
+//    print("\(movie.name): \(movie.cast.joined(separator: ", "))")
+//}
+
 // Populate an array of Movie structs converted from the familiar array of dictionaries.
 for movieDict in movies {
     if let movie = Movie(from: movieDict) {
